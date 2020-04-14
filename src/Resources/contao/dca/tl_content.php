4e1bc5395ecefcfcb7278227f63d777f562c5a0f
delirius <github@delirius.ch>
2020-04-14 15:40:03 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -12,6 +12,6 @@
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['divider'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['divider'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 
 ?>
