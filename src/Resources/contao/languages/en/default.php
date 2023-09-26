7735759926ff495e6da266012a88d6576edae72a
stefansl <ssl@clickpress.de>
2023-09-26 17:04:43 +0200

    Add en translation for the form widget

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,4 +1,6 @@
 <?php
+$GLOBALS['TL_LANG']['FFL']['divider'][0] = 'Horizontal Rule (Divider)';
+$GLOBALS['TL_LANG']['FFL']['divider'][1] = 'Generates a divider (hr)';
+
 $GLOBALS['TL_LANG']['CTE']['divider'][0] = 'Horizontal Rule (Divider)';
-$GLOBALS['TL_LANG']['CTE']['divider'][1] = 'Generates a divider (<hr>)';
-?>
+$GLOBALS['TL_LANG']['CTE']['divider'][1] = 'Generates a divider (hr)';
