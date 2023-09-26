7d851b8e47a0fc852f8b25b0c915fde298607254
stefansl <ssl@clickpress.de>
2023-09-26 17:05:20 +0200

    Fix #2

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -3,11 +3,10 @@
  * Form fields
  */
 $GLOBALS['TL_LANG']['FFL']['divider'][0] = 'Trennlinie';
-$GLOBALS['TL_LANG']['FFL']['divider'][1] = 'Erstellt eine Trennlinie (<hr>)';
+$GLOBALS['TL_LANG']['FFL']['divider'][1] = 'Erstellt eine Trennlinie (hr)';
 
 /**
  * Content Elements
  */
 $GLOBALS['TL_LANG']['CTE']['divider'][0] = 'Trennlinie';
-$GLOBALS['TL_LANG']['CTE']['divider'][1] = 'Erstellt eine Trennlinie (<hr>)';
-?>
+$GLOBALS['TL_LANG']['CTE']['divider'][1] = 'Erstellt eine Trennlinie (hr)';
